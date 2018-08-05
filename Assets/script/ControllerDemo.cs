4b9d006f378dcda242521c95fd9e239313f8ff98
BerryBless <shsuzakfn@gmail.com>
2018-08-05 12:22:19 +0900

    adding files

---
+++ Assets/script/ControllerDemo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerDemo : MonoBehaviour {
+    public string Button = "Button_A"; // 버튼 매핑된 값
+    private int cnt = 0; // 몇번 눌렸나
+
+    float Timer = 0;
+
+    void start(){
+        cnt = 0;
+        Timer = 0;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        Timer += Time.deltaTime;
+
+        if (Input.GetButtonDown(Button)) {
+            ++cnt;
+            Debug.Log( Button + " : " + cnt  + " times.. by " 
+                + Timer);
+            Timer = 0;
+ 
+        }
+	}
+}
